f5dbcdbd2527e019f7982943de1ddd3d57a6d614
SashaSerh <101942102+SashaSerh@users.noreply.github.com>
2023-01-09 21:08:33 +0200

    home work #7
    home work #7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description})=>{
+        return `<li><a class='gallery__link' href='${original}'>
+        <img  class='gallery__image' src ='${preview}' alt ='${description}'>
+        </a></li>`;}).join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
